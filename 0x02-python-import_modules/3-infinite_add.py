dc23908e35677f03df8b67ec2350072a22845b1e
sucre2020 <90860451+sucre2020@users.noreply.github.com>
2023-01-19 11:46:34 +0100

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+from sys import argv
+res = 0
+
 if __name__ == "__main__":
-    from sys import argv
-    n = len(argv)
-    add = 0
-    if n > 1:
-        for i in range(1, n):
-            add += int(argv[i])
-        print(add)
-    else:
-        print(0)
+    for i, j in enumerate(argv):
+        if i != 0:
+            res += int(j)
+    print("{:d}".format(res))
