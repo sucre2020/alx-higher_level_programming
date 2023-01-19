96f8349b86a7c413b02c2642ce70785fcf9fbbfc
sucre2020 <90860451+sucre2020@users.noreply.github.com>
2023-01-19 11:38:20 +0100

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
-
-a = 1
-b = 2
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
 if __name__ == "__main__":
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
