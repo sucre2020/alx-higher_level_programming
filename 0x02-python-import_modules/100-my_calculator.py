27e9ce8486ef6d3048af4e257397eae37203f9a6
sucre2020 <90860451+sucre2020@users.noreply.github.com>
2023-01-19 11:17:54 +0100

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
+    n = len(argv) - 1
+    if n != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if argv[2] != "+" and argv[2] != "-" and argv[2] != "*" and argv[2] != "/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+    if op == "+":
+        print(f"{a} + {b} = {add(a, b)}")
+    elif op == "-":
+        print(f"{a} - {b} = {sub(a, b)}")
+    elif op == "*":
+        print(f"{a} * {b} = {mul(a, b)}")
+    else:
+        print(f"{a} / {b} = {div(a, b)}")
