95a6e2b4dcf5c1d12b60302164565ffec0bfbb5f
Efita Effiom <90860451+sucre2020@users.noreply.github.com>
2023-01-31 00:08:04 +0100

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Python script that creates a class and functions for the class"""
+
+
+class Square:
+    """Class that defines a square with a private instance attribute size"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter and setter used to validate type and value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
+
+    def area(self):
+        """Returns the area of the square"""
+        return int(self.__size) * int(self.__size)
