31c37c8b4e042da2326eef22fff66111dcee0575
Efita Effiom <90860451+sucre2020@users.noreply.github.com>
2023-01-30 23:56:43 +0100

    101-square.py

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Python script that creates a class and functions for the class"""
+
+
+class Square:
+    """Class that defines a square with a private instance attribute size"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter and setter used to validate type and value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
+
+    @property
+    def position(self):
+        """Getter and setter used to validate type and value of position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        err = 'position must be a tuple of 2 positive integers'
+        if isinstance(value, tuple) and len(value) == 2 and value[0] >= 0\
+                and value[1] >= 0 and isinstance(value[0], int)\
+                and isinstance(value[1], int):
+                    self.__position = value
+        else:
+            raise TypeError(err)
+
+    def area(self):
+        """Returns the area of the square"""
+        return int(self.__size) * int(self.__size)
+
+    def my_print(self):
+        """Prints a square using '#'"""
+        for j in range(self.__position[1]):
+            print("")
+        if self.size == 0:
+            print("")
+        for i in range(self.size):
+            if self.__position[0] > 0:
+                print(" " * self.__position[0], end="")
+            print('#' * self.size)
