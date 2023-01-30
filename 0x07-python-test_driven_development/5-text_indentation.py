26f59dae4a5ee2bb6bea3de6f7435fbac8d9068f
Efita Effiom <90860451+sucre2020@users.noreply.github.com>
2023-01-31 00:33:03 +0100

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Print text indented module"""
+
+
+def text_indentation(text):
+    """
+    prints text followed by 2 new lines after ".", "?", or ":"
+
+    @text: the text to be printed
+    """
+    if not isinstance(text, str) or text is None or len(text) <= 0:
+        raise TypeError('text must be a string')
+    signal = 1
+    for i in text:
+        if i == '?' or i == ':' or i == '.':
+            print(i, end="\n\n")
+            signal = 0
+        else:
+            if signal == 1:
+                if i == '\t' or i == '\n' or i == '\v':
+                    pass
+                else:
+                    print(i, end="")
+            else:
+                if i == ' ' or i == '\t':
+                    pass
+                else:
+                    print(i, end="")
+                    signal = 1
