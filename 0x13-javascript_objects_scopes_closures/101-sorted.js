4996fba483295571426ffa525cf2431a34030ca9
Efita Effiom <90860451+sucre2020@users.noreply.github.com>
2023-03-15 15:59:13 -0600

    101-sorted.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+'use strict';
+
+const dict = require('./101-data').dict;
+
+const newDict = {};
+for (const key in dict) {
+const value = dict[key];
+if (!(value in newDict)) {
+newDict[value] = [];
+}
+newDict[value].push(key);
+}
+
+console.log(newDict);
